8e24f0ee93df7e8650eb881b8c828e8ceebbf77b
danek0100 <71225718+danek0100@users.noreply.github.com>
2022-10-04 19:09:12 +0300

    @Component

--- 1/src/main/java/org/webapp/ClassicalMusic.java
+++ 1/src/main/java/org/webapp/ClassicalMusic.java
@@ -1,6 +1,26 @@
 package org.webapp;
 
-public class ClassicalMusic implements Music{
+import org.springframework.stereotype.Component;
+
+@Component("someClassicalMusic")
+public class ClassicalMusic implements Music {
+
+    private ClassicalMusic() {
+
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    public void init() {
+        System.out.println("Initialization");
+    }
+
+    public void destroy() {
+        System.out.println("Destruction");
+    }
+
     @Override
     public String getSong() {
         return "Rhapsody";
--- 1/src/main/java/org/webapp/TestSpring.java
+++ 1/src/main/java/org/webapp/TestSpring.java
@@ -8,16 +8,20 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
+        ClassicalMusic classicalMusic = context.getBean("someClassicalMusic", ClassicalMusic.class);
+        RockMusic rockMusic = context.getBean("rockMusic", RockMusic.class);
+        System.out.println(classicalMusic.getSong());
+        System.out.println(rockMusic.getSong());
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        System.out.println(firstMusicPlayer==secondMusicPlayer);
+        //MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        //MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        //System.out.println(firstMusicPlayer==secondMusicPlayer);
 
-        secondMusicPlayer.setVolume(125);
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+        //secondMusicPlayer.setVolume(125);
+        //System.out.println(firstMusicPlayer.getVolume());
+        //System.out.println(secondMusicPlayer.getVolume());
         context.close();
     }
 }
