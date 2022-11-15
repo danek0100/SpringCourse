dd7fdd85017db32a7c647f3f0f5568bf35f71203
danek0100 <71225718+danek0100@users.noreply.github.com>
2022-11-15 17:59:08 +0300

    GET request with params

---
+++ 3/src/main/java/com/danek0100/springcourse/controllers/FirstController.java
@@ -0,0 +1,31 @@
+package com.danek0100.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello, " + name + " " + surname);
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbay")
+    public String goodByePage() {
+        return "first/goodbay";
+    }
+
+    @GetMapping("/exit")
+    public String exitPage() {
+        return "first/exit";
+    }
+}
