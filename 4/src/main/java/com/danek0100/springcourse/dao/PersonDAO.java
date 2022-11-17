43b1283d47743ef453f98be5a4800039edf2f0a3
danek0100 <71225718+danek0100@users.noreply.github.com>
2022-11-17 21:02:24 +0300

    CRUD without DB

---
+++ 4/src/main/java/com/danek0100/springcourse/dao/PersonDAO.java
@@ -0,0 +1,45 @@
+package com.danek0100.springcourse.dao;
+
+import com.danek0100.springcourse.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Georg", 55, "aha@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Senya", 123, "lod@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Katya", 20, "katy@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Danil", 22, "danilys@mail.ru"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
+}
